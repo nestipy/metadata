4d8757fa0d2414a57bffe489dba4e129436d0600
tsiresymila <tsiresymila@gmail.com>
2024-04-25 17:52:04 +0300

    add data fron dynamic module

--- src/nestipy_metadata/class_.py
+++ src/nestipy_metadata/class_.py
@@ -17,20 +17,24 @@ class ClassMetadata:
         return self._module
 
     def get_service_providers(self):
-        providers = self._global_providers + Reflect.get_metadata(self._module, ModuleMetadata.Providers, [])
+        providers = Reflect.get_metadata(self._module, ModuleMetadata.Providers, [])
         import_providers_form_exports = []
         # Only not a root module need to get import_providers to share
-        if not Reflect.get_metadata(self._module, ModuleMetadata.Root, False):
-            for im in Reflect.get_metadata(self._module, ModuleMetadata.Imports, []):
-                for export in Reflect.get_metadata(im, ModuleMetadata.Exports, []):
-                    # For module re-exporting
-                    is_module: bool = Reflect.get_metadata(export, ModuleMetadata.Module, False)
-                    if is_module:
-                        import_providers_form_exports = import_providers_form_exports + Reflect.get_metadata(
-                            export,
-                            ModuleMetadata.Providers,
-                            []
-                        )
-                    else:
-                        import_providers_form_exports.append(export)
+        # if not Reflect.get_metadata(self._module, ModuleMetadata.Root, False):
+        for im in Reflect.get_metadata(self._module, ModuleMetadata.Imports, []):
+            exports = Reflect.get_metadata(im.module if hasattr(im, 'module') else im, ModuleMetadata.Exports, [])
+            # check if dynamic module
+            # if hasattr(im, 'module') and hasattr(im, 'exports'):
+            #     exports = getattr(im, 'exports', [])
+            for export in exports:
+                # For module re-exporting
+                is_module: bool = Reflect.get_metadata(export, ModuleMetadata.Module, False)
+                if is_module:
+                    import_providers_form_exports = import_providers_form_exports + Reflect.get_metadata(
+                        export,
+                        ModuleMetadata.Providers,
+                        []
+                    )
+                else:
+                    import_providers_form_exports.append(export)
         return providers, import_providers_form_exports
